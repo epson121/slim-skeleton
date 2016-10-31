47a0a4c60881ea8451252d5183f043b136202a6c
epson121 <traxdater12@gmail.com>
2016-10-31 14:48:52 +0100

    Connected controller to base

--- src/Controllers/Index.php
+++ src/Controllers/Index.php
@@ -6,12 +6,12 @@ namespace Controllers;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-class Index
+class Index extends Base
 {
 
     public function index(Request $request, Response $response, array $args)
     {
-
+    		$this->render($response, 'index/index.phtml');
     }
 
 }
